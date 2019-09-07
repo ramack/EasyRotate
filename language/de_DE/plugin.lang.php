b3b3abfe20c0306fa76d1d2ece31f4a5946eb757
Raphael Mack <ramack@raphael-mack.de>
2019-09-07 22:06:16 +0200

    add German translation

--- language/de_DE/plugin.lang.php
+++ language/de_DE/plugin.lang.php
@@ -4,4 +4,5 @@ $lang['EasyRotate'] = 'EasyRotate';
 $lang['Rotate clockwise'] = 'im Uhrzeigersinn drehen';
 $lang['Rotate counterclockwise'] = 'gegen den Uhrzeigersinn drehen';
 $lang['Rotate HD Images'] = 'Auch HD Bilder drehen';
+$lang['To install this plugin, you need to install the rotateImage plugin first.'] = 'Um dieses Plugin zu installieren wird muss zuerst das verwendete rotateImage installiert werden.';
 ?>
