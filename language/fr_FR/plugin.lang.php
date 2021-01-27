339e9ad592652633420ac47d051cb19f0c812c6d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-01-27 23:15:06 +0100

    Update fr_FR, thanks to ddtddt

---
+++ language/fr_FR/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Rotate clockwise'] = 'Tourner dans le sens horaire ';
+$lang['Rotate counterclockwise'] = 'Tourner dans le sens trigonométrique';
