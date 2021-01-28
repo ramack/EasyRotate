38ad8c594fc33a9f34fcf7423dc162947f3aa7e0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-01-28 20:38:14 +0100

    Update lv_LV, thanks to agrisans

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['To install this plugin, you need to install the rotateImage plugin first.'] = 'Lai uzinstalētu šo spraudni, vispirms ir jāinstalē rotateImage spraudni';
+$lang['EasyRotate'] = 'Vieglā Pagriešana';
+$lang['Rotate HD Images'] = 'Pagriezt HD attēlus';
+$lang['Rotate counterclockwise'] = 'Pagriezt pret pulksteņrādītāju';
+$lang['Rotate clockwise'] = 'Pagriezt pulksteņrādītāja virzienā';
