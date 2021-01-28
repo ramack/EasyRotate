560187a039cd97b7f170334b6e16ea536d68b8c1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-01-28 06:26:37 +0100

    Update nb_NO, thanks to paulen

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['EasyRotate'] = 'Enkel rotering';
+$lang['Rotate HD Images'] = 'Roter HD-Bilder';
+$lang['Rotate clockwise'] = 'Roter med uret';
+$lang['Rotate counterclockwise'] = 'Roter mot uret';
+$lang['To install this plugin, you need to install the rotateImage plugin first.'] = 'For å installere dette programtillegget må du først installere programtillegget roter-bilde';
