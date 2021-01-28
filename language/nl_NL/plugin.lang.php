e8519e06c23680b5aa7e62e73bcec8914e7f75e6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-01-28 20:37:57 +0100

    Update nl_NL, thanks to Pat2309

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['EasyRotate'] = 'EasyRotate';
+$lang['Rotate HD Images'] = 'Draai HD Afbeeldingen';
+$lang['Rotate clockwise'] = 'Draai wijzerzin';
+$lang['Rotate counterclockwise'] = 'Draai tegen wijzerzin';
+$lang['To install this plugin, you need to install the rotateImage plugin first.'] = 'Om deze plugin te installeren, moet u eerst de plugin rotateImage installeren';
