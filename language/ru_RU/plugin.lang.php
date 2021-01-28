84c0f9df3fbb36a60803517bdc67f694f37c4730
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-01-28 07:33:42 +0100

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['EasyRotate'] = 'EasyRotate';
+$lang['Rotate HD Images'] = 'Поворот изображений HD';
+$lang['Rotate clockwise'] = 'Поворот по часовой стрелке';
+$lang['Rotate counterclockwise'] = 'Поворот против часовой стрелки';
+$lang['To install this plugin, you need to install the rotateImage plugin first.'] = 'Прежде чем установить этот плагин, Вы должны установить плагин rotateImage';
