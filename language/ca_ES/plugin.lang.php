8d3762de16f2355b111145001d821c811cdfbc6c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-01-28 11:30:38 +0100

    Update ca_ES, thanks to Tirs

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['EasyRotate'] = 'EasyRotate';
+$lang['Rotate HD Images'] = 'Rota imatges en HD';
+$lang['Rotate clockwise'] = 'Rota en el sentit de les busques del rellotge';
+$lang['Rotate counterclockwise'] = 'Rota en sentit contrari a les busques del rellotge';
+$lang['To install this plugin, you need to install the rotateImage plugin first.'] = 'Per instal·lar aquest connector, necessiteu instal·lar abans el connector rotateImage.';
