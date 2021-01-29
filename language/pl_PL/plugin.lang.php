6bbc5be160a666795973188d1bf47bbe0ab30063
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-01-29 16:43:06 +0100

    Update pl_PL, thanks to Sauron

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['EasyRotate'] = 'EasyRotate';
+$lang['Rotate HD Images'] = 'Obróć obrazy HD';
+$lang['Rotate clockwise'] = 'Obróć zgodnie z ruchem zegara';
+$lang['Rotate counterclockwise'] = 'Obróć przeciwnie do ruchu zegara';
+$lang['To install this plugin, you need to install the rotateImage plugin first.'] = 'Przed zainstalowaniem tej wtyczki, musisz zainstalować wcześniej wtyczkę rotateImage.';
