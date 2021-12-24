e6b35c8e29da03dd0e6e8d444c9780d6254e7bd1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-12-24 19:45:43 +0100

    Update sl_SI, thanks to maxima

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['EasyRotate'] = 'EasyRotate';
+$lang['Rotate HD Images'] = 'Zavrtite slike HD';
+$lang['Rotate clockwise'] = 'Zavrtite v smeri urinega kazalca';
+$lang['Rotate counterclockwise'] = 'Zavrtite v nasprotni smeri urinega kazalca';
+$lang['To install this plugin, you need to install the rotateImage plugin first.'] = 'Če želite namestiti ta vtičnik, morate najprej namestiti vtičnik rotateImage.';
