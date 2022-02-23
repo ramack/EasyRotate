14b7e8f07e601c51f87a0c9813fb6e1b8ff88601
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-02-23 08:07:19 +0100

    Update fr_FR, thanks to plg

--- language/fr_FR/plugin.lang.php
+++ language/fr_FR/plugin.lang.php
@@ -5,8 +5,8 @@
 // | For copyright and license information, please view the COPYING.txt    |
 // | file that was distributed with this source code.                      |
 // +-----------------------------------------------------------------------+
-$lang['Rotate clockwise'] = 'Tourner dans le sens horaire ';
-$lang['Rotate counterclockwise'] = 'Tourner dans le sens trigonométrique';
+$lang['Rotate clockwise'] = 'Tourner dans le sens des aiguilles d\'une montre ';
+$lang['Rotate counterclockwise'] = 'Tourner dans le sens inverse des aiguilles d\'une montre';
 $lang['EasyRotate'] = 'EasyRotate';
 $lang['Rotate HD Images'] = 'Faire pivoter les images HD';
 $lang['To install this plugin, you need to install the rotateImage plugin first.'] = 'Pour installer ce plugin, vous devez d’abord installer le plugin rotateImage.';
